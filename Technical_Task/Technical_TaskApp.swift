3e677182b0cce9e877f6ba66bb7bc5e2fbc72061
abkubatt <abkubatt@gmail.com>
2023-03-05 23:18:44 +0600

    Initial Commit

---
+++ Technical_Task/Technical_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Technical_TaskApp.swift
+//  Technical_Task
+//
+//  Created by Abdulmajit Kubatbekov on 05.03.23.
+//
+
+import SwiftUI
+
+@main
+struct Technical_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
